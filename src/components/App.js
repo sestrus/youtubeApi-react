ce4de1b9a61ca0fdc7690d116fc2b1ec544cf346
Piotr <piotr.burdzy.klb@gmail.com>
2022-09-01 12:13:58 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+
+class App extends React.Component {
+  state = { videos: [] };
+
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
